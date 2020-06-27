a43876951b219bf6b689731fa36781d967f0a6a4
svmihar <svmihar@gmail.com>
2020-06-27 12:55:03 +0700

    :ambulance: fixing error link on dataset using parsaer

--- _uploader.py
+++ _uploader.py
@@ -1,6 +1,13 @@
 import os
 import time
 import logging
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-t', '--time', type=int, default=11,
+                    help='mau berapa kali diulang dalam setengah jam?')
+args = parser.parse_args()
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
@@ -16,6 +23,6 @@ while True:
     logger.info("sleeping for 30 minutes")
     time.sleep(1800)
     os.system("drive add_remote")
-    if c > 11:
+    if c > args.time:
         os.chdir("../../")
         os.system("python finish_me.py")
--- start_me.py
+++ start_me.py
@@ -1,10 +1,16 @@
 import os
+import argparse
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-l', '--link', type=str, default='https://drive.google.com/uc?id=1HMkzNpiUrndGyvVqsvKBbD3yTkjOnydH',
+                    help='link google drive datasetnya ')
+args = parser.parse_args()
 
 commands = [
     "mkdir dataset",
     "pip install drive-cli",
-    "gdown https://drive.google.com/uc?id=1HMkzNpiUrndGyvVqsvKBbD3yTkjOnydH",
+    f"gdown {args.link}",
     'unzip "Copy of shopee-product-detection-dataset.zip" -d dataset/',
     'rm -rf "Copy of shopee-product-detection-dataset.zip" ',
     "mv dataset/test/test/* dataset/test/",
