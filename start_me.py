f387fd3575c76d929025b630fa69a3148507e9a2
svmihar <svmihar@gmail.com>
2020-06-27 12:46:34 +0700

    :art: improving recurring codes

--- start_me.py
+++ start_me.py
@@ -1,10 +1,17 @@
 import os
 
-os.system("mkdir dataset")
-os.system("gdown https://drive.google.com/uc?id=1HMkzNpiUrndGyvVqsvKBbD3yTkjOnydH")
-os.system('unzip "Copy of shopee-product-detection-dataset.zip" -d dataset/')
-os.system('rm -rf "Copy of shopee-product-detection-dataset.zip" ')
-os.system("mv dataset/test/test/* dataset/test/")
-os.system("mv dataset/train/train/* dataset/train/")
-os.system("rm -rf dataset/test/test")
-os.system("rm -rf dataset/train/train")
+
+commands = [
+    "mkdir dataset",
+    "pip install drive-cli",
+    "gdown https://drive.google.com/uc?id=1HMkzNpiUrndGyvVqsvKBbD3yTkjOnydH",
+    'unzip "Copy of shopee-product-detection-dataset.zip" -d dataset/',
+    'rm -rf "Copy of shopee-product-detection-dataset.zip" ',
+    "mv dataset/test/test/* dataset/test/",
+    "mv dataset/train/train/* dataset/train/",
+    "rm -rf dataset/test/test",
+    "rm -rf dataset/train/train",
+]
+
+for cmd in commands:
+    os.system(cmd)
