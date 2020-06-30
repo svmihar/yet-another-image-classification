1ed7d0d22b1f477659d5b9c2481357c60a91b76c
svmihar <svmihar@gmail.com>
2020-06-30 06:59:10 +0000

    :zap: training failed on jupyter notebook, WIP training script

--- finish_me.py
+++ finish_me.py
@@ -8,8 +8,8 @@ parser.add_argument("-t", "--time", type=int, default=0, help="waktu sleep")
 args = parser.parse_args()
 
 if args.time > 0:
-    print(f"sleeping for {args.time} seconds")
-    time.sleep(args.time)
+    print(f"sleeping for {args.time*3600} seconds")
+    time.sleep(args.time*3600)
 
 training_folder = "./dataset"
 uploader()
